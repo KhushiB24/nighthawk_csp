ba7a8cbdff7f944a297e286510d5fc65755ae517
khush <khushibagri24@gmail.com>
2022-04-07 12:35:29 -0700

    file deletion

--- __init__.py
+++ __init__.py
@@ -1,23 +1,29 @@
-from flask import Flask
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Blueprint, render_template
 
-"""
-These object will be used throughout project.
-1.) Objects from this file can be included in many blueprints
-2.) Isolating these object definitions avoids duplication and circular dependencies
-"""
+app_y2022 = Blueprint('y2022', __name__,
+                      url_prefix='/y2022',
+                      template_folder='templates',
+                      static_folder='static', static_url_path='/static/assets')
 
-# Setup of key Flask object (app)
-app = Flask(__name__)
-dbURI = 'sqlite:///model/myDB.db'
-# Setup SQLAlchemy object and properties for the database (db)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
-app.config['SECRET_KEY'] = 'SECRET_KEY'
-db = SQLAlchemy(app)
-Migrate(app, db)
-# Setup LoginManager object (app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+
+@app_y2022.route('/github')
+def github():
+    return render_template("course/github.html")
+
+@app_y2022.route('/replit')
+def replit():
+    return render_template("course/replit.html")
+
+@app_y2022.route('/deploy')
+def deploy():
+    return render_template("course/deploy.html")
+
+
+@app_y2022.route('/tri1')
+def tri1():
+    return render_template("course/timelines.html", padlet="https://padlet.com/jmortensen7/csp2022tri1")
+
+
+@app_y2022.route('/tri2')
+def tri2():
+    return render_template("course/timelines.html", padlet="https://padlet.com/jmortensen7/csp2022tri2")
